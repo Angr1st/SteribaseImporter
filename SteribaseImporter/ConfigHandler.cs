63a746a25a05f962e61b5fadd1226016ab02099e
Torben Clasen <torbenclasen@gmail.com>
2018-05-22 22:34:48 +0200

    Createt a ConfigHandler that return all possible config values.

--- SteribaseImporter/ConfigHandler.cs
+++ SteribaseImporter/ConfigHandler.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Text;
 
 namespace SteribaseImporter
 {
+    public enum ConfigValues
+    {
+        newFolder,
+        processedFolder,
+        failedFolder,
+        order
+    }
+
     class ConfigHandler
     {
+        public static readonly string newFolder = "newFolder";
+        public static readonly string processedFolder = "processedFolder";
+        public static readonly string failedFolder = "failedFolder";
+        public static readonly string order = "order";
+
+        public static string GetConfigValue(ConfigValues configValue) => ConfigurationManager.AppSettings[configValue.ToString()];
     }
 }
