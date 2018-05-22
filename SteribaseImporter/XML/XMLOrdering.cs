44a4ce31bd8659476cc9258b8013530307d3355e
Torben Clasen <torbenclasen@gmail.com>
2018-05-22 22:46:36 +0200

    Created a class to load the ordering-file

--- SteribaseImporter/XML/XMLOrdering.cs
+++ SteribaseImporter/XML/XMLOrdering.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Linq;
 
 namespace SteribaseImporter.XML
 {
     class XMLOrdering
     {
+        public IEnumerable<string> GetOrderingList(string filePath) => LoadLines(filePath);
+
+        private static IEnumerable<string> LoadLines(string fileName)
+        {
+            try
+            {
+                return File.ReadAllLines(fileName).Select(line => line.TrimEnd(';'));
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error while loading the XML.", e);
+            }
+        }
     }
 }
