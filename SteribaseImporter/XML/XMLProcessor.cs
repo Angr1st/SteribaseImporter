e4022c1f934ed32213f73a65fd2e79349334b55c
Torben Clasen <torbenclasen@gmail.com>
2018-05-23 17:35:21 +0200

    Updated to Language Version 7.3

--- SteribaseImporter/XML/XMLProcessor.cs
+++ SteribaseImporter/XML/XMLProcessor.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml;
 
 namespace SteribaseImporter.XML
 {
     class XMLProcessor
     {
+       public (int erfolgreich, int fehlerhaft) ImportXml(XmlDocument xmlDocument)
+        {
+
+            return default;
+        }
+
     }
 }
