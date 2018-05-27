5506aefd842cdf6a7cd67d41073cb15d5b140403
Torben Clasen <torbenclasen@gmail.com>
2018-05-27 14:47:19 +0200

    Intermediate Commit (TODO: Finish loading db structure and parsing into DBTables)

--- SteribaseImporter/DB/DBStructureLoader.cs
+++ SteribaseImporter/DB/DBStructureLoader.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace SteribaseImporter.DB
 {
     class DBStructureLoader
     {
-
+        public List<DBTable> LoadDBStructure()
+        {
+            File.ReadAllLines(ConfigHandler.GetConfigValue(ConfigValues.dbstructure)).Select(Line => Line.Split(';').Select(subLine => subLine.Split(',')))
+        }
     }
 }
