06a449f9d3b78363da5f6784b23e2e5acb499ab3
Torben Clasen <torbenclasen@gmail.com>
2018-05-29 22:29:53 +0200

    Added all methods to set the value of the DBFieldEntry

--- SteribaseImporter/DB/DBFieldEntry.cs
+++ SteribaseImporter/DB/DBFieldEntry.cs
@@ -7,14 +7,52 @@ namespace SteribaseImporter.DB
     class DBFieldEntry
     {
         public DateTimeOffset DateTimeOffset { get; private set; }
-        public int Int { get; }
-        public Double Double { get; }
-        public string String { get; }
+        public int Int { get; private set; }
+        public Double Double { get; private set; }
+        public string String { get; private set; }
+        public int Length { get; private set; }
         public DBFieldType DBFieldType { get; }
 
         public DBFieldEntry(DBFieldType fieldType = DBFieldType.unkown)
         {
             DBFieldType = fieldType;
+        }
+
+        public DBFieldEntry SetDateTimeOffset(DateTimeOffset dateTimeOffset)
+        {
+            if (DBFieldType.HasFlag(DBFieldType.DateTime))
+            {
+                DateTimeOffset = dateTimeOffset;
+            }
+            return this;
+        }
+
+        public DBFieldEntry SetInt(int value)
+        {
+            if (DBFieldType.HasFlag(DBFieldType.@int))
+            {
+                Int = value;
+            }
+            return this;
+        }
+
+        public DBFieldEntry SetDouble(double value)
+        {
+            if (DBFieldType.HasFlag(DBFieldType.@double))
+            {
+                Double = value;
+            }
+            return this;
+        }
+
+        public DBFieldEntry SetString(string value, int length = 200)
+        {
+            if (DBFieldType.HasFlag(DBFieldType.varchar) && value.Length <= length)
+            {
+                String = value;
+                Length = length;
+            }
+            return this;
         }
     }
 }
