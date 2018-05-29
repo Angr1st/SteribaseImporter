bf63e46410ca46a3fc6fc87179becd8ea70670eb
Torben Clasen <torbenclasen@gmail.com>
2018-05-29 22:39:47 +0200

    Added a static class for extension of the XmlNodeList

---
+++ SteribaseImporter/XML/XmlNodeListExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace SteribaseImporter.XML
+{
+    static class XmlNodeListExtension
+    {
+        public static List<XmlNode> ToXmlNodeList(this XmlNodeList nodeList)
+        {
+            var elementList = new List<XmlNode>();
+            foreach (XmlNode item in nodeList)
+            {
+                elementList.Add(item);
+            }
+            return elementList;
+        }
+    }
+}
