0d0d6d81750fd91c2506c4d7be7fd878da986e7d
Torben Clasen <torbenclasen@gmail.com>
2018-06-01 20:29:20 +0200

    Added tostring overide

--- SteribaseImporter/DB/DBFieldEntry.cs
+++ SteribaseImporter/DB/DBFieldEntry.cs
@@ -56,5 +56,10 @@ namespace SteribaseImporter.DB
             }
             return this;
         }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
     }
 }
