39b629a8633eeca61e8e0f56f938c0acb75d9701
Torben Clasen <torbenclasen@gmail.com>
2018-06-02 23:05:45 +0200

    Implementing Moving the processed Files into the processed folder

--- SteribaseImporter/Program.cs
+++ SteribaseImporter/Program.cs
@@ -3,6 +3,9 @@ using SteribaseImporter.XML;
 using System.Linq;
 using System.Configuration;
 
+using static System.Console;
+using System.IO;
+
 namespace SteribaseImporter
 {
     class Program
@@ -10,12 +13,12 @@ namespace SteribaseImporter
         static void Main(string[] args)
         {
             DB.DBStructureLoader dBStructureLoader = new DB.DBStructureLoader();
-           var result = dBStructureLoader.LoadDBStructure();
+            var result = dBStructureLoader.LoadDBStructure();
             XMLMover mover = new XMLMover();
             XMLOrdering ordering = new XMLOrdering();
-            XMLProcessor processor = new XMLProcessor(ordering.GetOrderingList(ConfigHandler.GetConfigValue(ConfigValues.order)), result,new MySql.Data.MySqlClient.MySqlConnection(ConfigurationManager.ConnectionStrings["steribaseDB"].ConnectionString));
-            var importResults = mover.LoadAllNewXmls().Select(doc => processor.ImportXml(doc)).ToList();
-            Console.WriteLine("Hello World!");
+            XMLProcessor processor = new XMLProcessor(ordering.GetOrderingList(ConfigHandler.GetConfigValue(ConfigValues.order)), result, new MySql.Data.MySqlClient.MySqlConnection(ConfigurationManager.ConnectionStrings["steribaseDB"].ConnectionString));
+            var importResults = mover.LoadAllNewXmls().Select(doc => (processor.ImportXml(doc.xmlDoc), doc.filePath)).Select(import => { WriteLine($"Import of {mover.GetFileName(import.filePath)}; Successfull:{import.Item1.erfolgreich}; Failed:{import.Item1.fehlerhaft}"); return import; }).Select(import => mover.MoveFile(import.filePath)).ToList();
+            ReadLine();
         }
     }
 }
--- SteribaseImporter/XML/XMLMover.cs
+++ SteribaseImporter/XML/XMLMover.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Xml;
 
 namespace SteribaseImporter.XML
@@ -16,9 +16,24 @@ namespace SteribaseImporter.XML
             NewFolder = ConfigHandler.GetConfigValue(ConfigValues.newFolder);
         }
 
-        public List<XmlDocument> LoadAllNewXmls()
+        public List<(XmlDocument xmlDoc, string filePath)> LoadAllNewXmls()
         {
-            return Directory.GetFiles(NewFolder).Select(file => { var xmlDoc = new XmlDocument(); xmlDoc.Load(file); return xmlDoc; }).ToList();
+            return Directory.GetFiles(NewFolder).Select(file => { var xmlDoc = new XmlDocument(); xmlDoc.Load(file); return (xmlDoc, file); }).ToList();
         }
+
+
+        public string MoveFile(string filePath)
+        {
+            var returnValue = Path.Combine(ConfigHandler.GetConfigValue(ConfigValues.processedFolder), GetFileName(filePath));
+            File.Move(filePath,returnValue);
+            return returnValue;
+        }
+
+        public string CreateErrorLog((int erfolgreich, int fehlerhaft, List<(MySqlCommand command, string message)> failedCommand) results)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFileName(string filePath) => filePath.Split(Path.DirectorySeparatorChar).Last();
     }
 }
