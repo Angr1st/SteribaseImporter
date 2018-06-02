311a0b97b0b7008e13d7685fe121f034807973b9
Torben Clasen <torbenclasen@gmail.com>
2018-06-02 17:11:54 +0200

    Changed the Ways simple string values are outputted

--- SteribaseImporter/DB/DBFieldEntry.cs
+++ SteribaseImporter/DB/DBFieldEntry.cs
@@ -66,7 +66,7 @@ namespace SteribaseImporter.DB
                 switch (DBFieldType)
                 {
                     case DBFieldType.varchar:
-                        return $"'{String}'";
+                        return String;
 
                     case DBFieldType.@int:
                         return Int.ToString();
